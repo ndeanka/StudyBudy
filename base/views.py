c547b688bcbc585c218177af984b18eda3001768
William Mlula <williammlula4@gmail.com>
2022-09-10 09:54:06 +0300

    login form authentication

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -18,13 +19,21 @@ from .forms import RoomForm
 
 def loginPage(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username') 
         password = request.POST.get('password')
         
         try:
             user = User.objects.get(username = username)
         except:
             messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
 
     context={}
     return render(request, 'base/login_register.html', context)
