bec88e1ab40f07a69176d88d740cf0501b94df09
William Mlula <williammlula4@gmail.com>
2022-09-10 23:04:43 +0300

    user registration

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -19,8 +20,12 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
-        username = request.POST.get('username') 
+        username = request.POST.get('username').lower() 
         password = request.POST.get('password')
         
         try:
@@ -36,13 +41,31 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password is incorrect')
 
-    context={}
+    context={'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    # page ='register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Error accorred during registration process')
+
+    context = {'form': form}
+    return render(request, 'base/login_register.html',context)
 
 
 def home(request):
